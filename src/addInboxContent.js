3d636b3c1a5cd28ecebf6f3713a8dfb321c76079
Artem <161304626+RTJIL@users.noreply.github.com>
2025-05-21 14:16:43 +0000

    add inbox layout

---
+++ src/addInboxContent.js
@@ -0,0 +1,10 @@
+function addInboxContent() {
+  const taskContent = document.querySelector(".taskContent")
+
+  const addInboxContainer = document.createElement("div")
+  addInboxContainer.classList.add("inboxContainer")
+
+  
+}
+
+export { addInboxContent }
